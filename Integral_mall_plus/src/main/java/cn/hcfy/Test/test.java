158438fb6b2faea807c1face04b7798c084e7534
chao_chao <936484194@qq.com>
2018-10-18 09:17:51 +0800

    增加后台模块

--- Integral_mall_plus/src/main/java/cn/hcfy/Test/test.java
+++ Integral_mall_plus/src/main/java/cn/hcfy/Test/test.java
@@ -7,10 +7,5 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class test {
     public static void main(String[] args) {
 
-        //加载xml文件
-        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        UserMapper mapper = (UserMapper) ac.getBean("UserMapper");
-
-        System.out.println(mapper);
     }
 }
